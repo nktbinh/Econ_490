477aef4f9699cfd8c1cbaf1cf37ef4bfa1fad2c8
Binh Nguyen <nguyenkhacthanhbinh214@gmail.com>
2017-08-15 20:22:27 -0600

    Edit factor recode

--- 1-Data Cleaning.R
+++ 1-Data Cleaning.R
@@ -1,8 +1,6 @@
 library(stringr) # Deal with string
-library(magrittr) # Pipe operators
 library(tidyverse) # General Purpose Data Cleaning
 library(forcats) # Deal with factors
-
 
 load("data.rda")
 binh<-da36361.0001
@@ -19,16 +17,18 @@ count_missing<-function (df){
   colnames(count)<-colnames(df)
   return(count)
 }
-count_missing(binh2)
 
 # Rename
-old_names<-c("K6SCMON","K6SCMAX","OTHINS","PRVHLTIN","HEALTH2","IRSEX","EMPSTATY","INCOME","EDUCCAT2","IREDUC2","IRMARIT","SEXRACE","NEWRACE2","CATAGE",
-             "DEPNDALC","ABUSEALC","TXNEDALC","ABODALC","ALCYRTOT","BINGEHVY","ALDAYPWK","ALCTRY","CIGTRY",
-             "POVERTY2","WHODASC3","AMDELT","ADPSSOC","WRKOCUP2","UADPEOP")
+old_names<-c("K6SCMON","K6SCMAX","OTHINS","PRVHLTIN","HEALTH2","IRSEX","EMPSTATY",
+             "INCOME","EDUCCAT2","IREDUC2","IRMARIT","SEXRACE","NEWRACE2","CATAGE",
+             "DEPNDALC","ABUSEALC","TXNEDALC","ABODALC","ALCYRTOT","BINGEHVY",
+             "ALDAYPWK","ALCTRY","CIGTRY", "POVERTY2","WHODASC3","AMDELT",
+             "ADPSSOC","WRKOCUP2","UADPEOP")
 
-new_names<-c("k6month","k6max","otherins","privateins","health","gender","empstat","inc","educat","educ","maristat","sexrace","race","age",
-             "alcdep","alcabu","alctre","abudep","daysalc","alcbin","alcweek","alctry","cigtry","pov","who",
-             "mde","feelsoc","typejob","alcsocial")
+new_names<-c("k6month","k6max","otherins","privateins","health","gender","empstat",
+             "inc","educat","educ","maristat","sexrace","race","age","alcdep",
+             "alcabu","alctre","abudep","daysalc","alcbin","alcweek","alctry",
+             "cigtry","pov","who","mde","feelsoc","typejob","alcsocial")
 
 binh2<-binh%>%
   select(one_of(old_names))
@@ -38,21 +38,20 @@ colnames(binh2)<-new_names
 binh2<-binh2 %>%
   mutate(age=fct_recode(age,
                         "Other"="(1) 12-17 Years Old",
-                        "18-25"="(2) 18-25 Years Old",
+                        "18 - 25"="(2) 18-25 Years Old",
                         "Other"="(3) 26-34 Years Old",
                         "Other"="(4) 35 or Older"))
 binh2$age<-relevel(binh2$age, ref="Other")
-binh2%>%count(age)
 
 # Deal with Health
 binh2<-binh2 %>%
   mutate(health=fct_recode(health,
-                        "Good/Very Good/Excellent"="(1) Excellent (HEALTH=1)",
-                        "Good/Very Good/Excellent"="(2) Very Good (HEALTH=2)",
-                        "Good/Very Good/Excellent"="(3) Good (HEALTH=3)",
+                        "Good/Excellent"="(1) Excellent (HEALTH=1)",
+                        "Good/Excellent"="(2) Very Good (HEALTH=2)",
+                        "Good/Excellent"="(3) Good (HEALTH=3)",
                         "Fair/Poor"="(4) Fair/Poor (HEALTH=4,5)"))%>%
   filter(health!="NA")
-binh2%>%count(health)
+binh2$health<-relevel(binh2$health, ref = "Fair/Poor")
 
 # Deal with Insurance
 binh2<-binh2%>%filter(privateins!="NA")
@@ -61,7 +60,6 @@ insured<-ifelse(binh2$otherins=="(1) Yes (IRMEDICR=1 or IRCHMPUS=1 or IROTHHLT=1
 binh2$insured<-insured
 binh2$insured<-as.factor(binh2$insured)
 binh2$insured<-relevel(binh2$insured, ref="Yes")
-binh2%>%count(insured)
 
 # Deal with Psychological Distress Variable (k6)
 binh2<-binh2%>%filter(k6month!="NA")%>%filter(k6max!="NA")
@@ -84,21 +82,18 @@ binh2<-binh2 %>%
                         "> $20,000"="(3) $50,000 - $74,999",
                         "> $20,000"="(4) $75,000 or More"))
 binh2$inc<-relevel(binh2$inc, ref="< $20,000")
-binh2%>%count(inc)
 
 # Deal with alcdep
 binh2<-binh2 %>%
   mutate(alcdep=fct_recode(alcdep,
-                           "No/Unknown"="(0) No/Unknown (Otherwise)",
+                           "No"="(0) No/Unknown (Otherwise)",
                            "Yes"="(1) Yes (See comment above)"))
-binh2%>%count(alcdep)
 
 # Deal with abudep
 binh2<-binh2 %>%
   mutate(abudep=fct_recode(abudep,
-                           "No/Unknown"="(0) No/Unknown (ABUSEALC=0 and DEPNDALC=0)",
+                           "No"="(0) No/Unknown (ABUSEALC=0 and DEPNDALC=0)",
                            "Yes"="(1) Yes (ABUSEALC=1 or DEPNDALC=1)"))
-binh2%>%count(abudep)
 
 # Deal with alcbin
 binh2<-binh2 %>%
@@ -107,26 +102,23 @@ binh2<-binh2 %>%
                            "Binge But Not Heavy Use"="(2) 'Binge' But Not Heavy Use (BINGEDRK=1 & HVYDRK2=0)",
                            "Past Month But Not Binge"="(3) Past Month But Not 'Binge' (ALCMON=1 & BINGEDRK=0)",
                            "No Alcohol Use Last Month"="(4) Did Not Use Alcohol in Past Month (All source=0)"))
-binh2%>%count(alcbin)
 
 # Deal with educat
 binh2<-binh2 %>%
   mutate(educat=fct_recode(educat,
-                           "< 12th Grade"="(1) Less than high school (IREDUC2<=7 and AGE2>=7)",
-                           "< 12th Grade"="(2) High school graduate (IREDUC2=8 and AGE2>=7)",
-                           "Finished 12th Grade"="(3) Some college (IREDUC2=9-10 and AGE2>=7)",
-                           "Finished 12th Grade"="(4) College graduate (IREDUC2=11 and AGE2>=7)",
-                           "< 12th Grade"="(5) 12 to 17 year olds (AGE2<=6)"))
-binh2%>%count(educat)
+                           "Highschool or Less"="(1) Less than high school (IREDUC2<=7 and AGE2>=7)",
+                           "Highschool or Less"="(2) High school graduate (IREDUC2=8 and AGE2>=7)",
+                           "Highschool & Greater"="(3) Some college (IREDUC2=9-10 and AGE2>=7)",
+                           "Highschool & Greater"="(4) College graduate (IREDUC2=11 and AGE2>=7)",
+                           "Highschool or Less"="(5) 12 to 17 year olds (AGE2<=6)"))
 
 # Deal with maristat
 binh2<-binh2 %>%
   mutate(maristat=fct_recode(maristat,
                              "Married"="(1) Married",
-                             "Unmarried"="(2) Widowed",
-                             "Unmarried"="(3) Divorced or Separated",
-                             "Unmarried"="(4) Never Been Married"))
-binh2%>%count(maristat)
+                             "Not Married"="(2) Widowed",
+                             "Not Married"="(3) Divorced or Separated",
+                             "Not Married"="(4) Never Been Married"))
 
 # Deal with sexrace
 binh2<-binh2 %>%
@@ -143,13 +135,13 @@ binh2<-binh2 %>%
 binh2<-binh2 %>%
   mutate(race=fct_recode(race,
                          "White"="(1) NonHisp White",
-                         "Other"="(2) NonHisp Black/Afr Am",
-                         "Other"="(3) NonHisp Native Am/AK Native",
-                         "Other"="(4) NonHisp Native HI/Other Pac Isl",
-                         "Other"="(5) NonHisp Asian",
-                         "Other"="(6) NonHisp more than one race",
-                         "Other"="(7) Hispanic"))
-binh2$race<-relevel(binh2$race, ref="Other")
+                         "Non-white"="(2) NonHisp Black/Afr Am",
+                         "Non-white"="(3) NonHisp Native Am/AK Native",
+                         "Non-white"="(4) NonHisp Native HI/Other Pac Isl",
+                         "Non-white"="(5) NonHisp Asian",
+                         "Non-white"="(6) NonHisp more than one race",
+                         "Non-white"="(7) Hispanic"))
+binh2$race<-relevel(binh2$race, ref="Non-white")
 binh2%>%count(race)
 
 # Deal with gender
@@ -162,4 +154,4 @@ binh2<-binh2 %>%
 binh2<-binh2%>%
   select(age,race,inc,health,maristat,insured,educat,k6,abudep,alcbin,daysalc,alcweek,everything())
 
-
+write.csv(binh2,"alcohol.csv", row.names = FALSE)
